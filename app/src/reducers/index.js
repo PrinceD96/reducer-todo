454e84df5919da2151e02f80a2c407060d565f18
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-01-31 10:41:12 -0500

    Built a reducer and initial state.

---
+++ app/src/reducers/index.js
@@ -0,0 +1,19 @@
+const todo = [
+	{
+		item: "Build a simple reducer",
+		completed: false,
+		id: 5648365987
+	},
+	{
+		item: "Set up state",
+		completed: false,
+		id: 0987654321
+	}
+];
+
+export const todoReducer = (state, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
